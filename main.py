539a1d295ec891ec3a648e12ed56b2676e083ff8
Sergey Postnikov <ser.post2017@gmail.com>
2023-06-03 17:57:01 +0300

    polling taken

--- main.py
+++ main.py
@@ -15,8 +15,19 @@ def get_reviews(api_key):
     return response.json()
 
 
+def get_polling(api_key):
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': f'Token {api_key}'}
+    response = requests.get(
+        url, 
+        headers=headers
+        )
+    response.raise_for_status()
+    return response.json()
+
+
 if __name__ == '__main__':
     load_dotenv()
     api_key = os.getenv('DVMN_API_KEY')
-    reviews = get_reviews(api_key)
-    pprint(reviews)
+    polling = get_polling(api_key)
+    pprint(polling)
