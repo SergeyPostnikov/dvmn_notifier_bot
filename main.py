3550958f6a7bb07a93c554cb4c2f07e10092c81b
Sergey Postnikov <ser.post2017@gmail.com>
2023-06-07 14:31:07 +0300

    handled with timestamp

--- main.py
+++ main.py
@@ -10,41 +10,50 @@ def get_reviews(api_key):
     url = 'https://dvmn.org/api/user_reviews/'
     headers = {'Authorization': f'Token {api_key}'}
     response = requests.get(
-        url, 
-        headers=headers
-        )
+        url,
+        headers=headers,
+    )
     response.raise_for_status()
     return response.json()
 
 
-def get_polling(api_key):
+def get_polling(api_key, last_attempt_timestamp):
     url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': f'Token {api_key}'}
-    while True:
-        response = requests.get(
-            url, 
-            headers=headers,
-            timeout=60
-            )
-        response.raise_for_status()
-        pprint(response.json())
+    payload = {'timestamp': last_attempt_timestamp}
+    response = requests.get(
+        url,
+        headers=headers,
+        params=payload,
+        timeout=91
+    )
+    response.raise_for_status()
+    return response.json()
 
 
 def main():
     load_dotenv()
     api_key = os.getenv('DVMN_API_KEY')
-    
-    try:
-        get_polling(api_key)
-    except ReadTimeout:
-        print('Polling time eceeded')
-    except ConnectionError as err:
-        for n in range(3):
-            sleep(30)
-            if n <= 3: 
-                get_polling(api_key)
+    last_attempt_timestamp = None
+    while True:
+        try:
+            response = get_polling(api_key, last_attempt_timestamp)
+            if response["status"] == "timeout":
+                last_attempt_timestamp = response["timestamp_to_request"]
             else:
-                print(err)
+                last_attempt_timestamp = response["last_attempt_timestamp"]
+                response = get_polling(api_key, last_attempt_timestamp)
+                pprint(response)
+        except ReadTimeout:
+            print('Polling time eceeded')
+        except ConnectionError as err:
+            for n in range(3):
+                sleep(30)
+                if n <= 3:
+                    response = get_polling(api_key, last_attempt_timestamp)
+                    pprint(response)
+                else:
+                    print(err)
 
 
 main()
