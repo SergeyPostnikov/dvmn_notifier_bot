37f38c78353079b4742b9afd6591aebc4f034c83
Sergey Postnikov <ser.post2017@gmail.com>
2023-06-08 18:08:54 +0300

    change seat commit

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from time import sleep
 from dvmn_handlers import get_polling
 import asyncio
 import telegram
-# import pprint
+import pprint
+
+
+async def get_message(response):
+    return pprint.pformat(response)
 
 
 async def send_message(bot, tg_user_id, response):
-    # message_text = pprint.pformat(response)
-    message = await bot.send_message(
-        text='Tutor checked your task!',
-        chat_id=tg_user_id)
-    print(message)
+    message_text = await get_message(response)
+    await bot.send_message(
+        text=message_text,
+        chat_id=tg_user_id
+    )
+    print(message_text)
 
 
 async def main():
@@ -31,18 +36,18 @@ async def main():
                 last_attempt_timestamp = response["timestamp_to_request"]
             else:
                 last_attempt_timestamp = response["last_attempt_timestamp"]
-                response = get_polling(api_key, last_attempt_timestamp)
+                # response = get_polling(api_key, last_attempt_timestamp)
                 await send_message(bot, tg_user_id, response)
         except ReadTimeout:
             print('Polling time exceeded')
-        except ConnectionError as err:
-            for n in range(3):
-                sleep(30)
-                if n <= 3:
-                    response = get_polling(api_key, last_attempt_timestamp)
-                    await send_message(bot, tg_user_id, response)
-                else:
-                    print(err)
+        # except ConnectionError as err:
+        #     for n in range(3):
+        #         sleep(30)
+        #         if n <= 3:
+        #             response = get_polling(api_key, last_attempt_timestamp)
+        #             await send_message(bot, tg_user_id, response)
+        #         else:
+        #             print(err)
 
 
 asyncio.run(main())
