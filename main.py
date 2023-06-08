acd567adbea842d07af4764190b34c0be97c448c
Sergey Postnikov <ser.post2017@gmail.com>
2023-06-08 21:47:08 +0300

    add custom message

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ from time import sleep
 from dvmn_handlers import get_polling
 import asyncio
 import telegram
-import pprint
 
 
 async def get_message(response):
-    return pprint.pformat(response)
+    attempt = response['new_attempts'][0]
+    title = attempt['lesson_title']
+    link = attempt['lesson_url']
+    is_negative = (attempt['is_negative'] == "False")
+    message = \
+        f'Your lesson <a href="{link}">"{title}"</a> \
+        was valdated with result - ' + \
+        ' "approved"' * is_negative +\
+        ' "not approved"' * (not is_negative)
+    return message
 
 
 async def send_message(bot, tg_user_id, response):
     message_text = await get_message(response)
     await bot.send_message(
         text=message_text,
-        chat_id=tg_user_id
+        chat_id=tg_user_id,
+        parse_mode='html'
     )
     print(message_text)
 
@@ -35,19 +44,19 @@ async def main():
             if response["status"] == "timeout":
                 last_attempt_timestamp = response["timestamp_to_request"]
             else:
-                last_attempt_timestamp = response["last_attempt_timestamp"]
-                # response = get_polling(api_key, last_attempt_timestamp)
                 await send_message(bot, tg_user_id, response)
+                last_attempt_timestamp = response["last_attempt_timestamp"]
+                response = get_polling(api_key, last_attempt_timestamp)
         except ReadTimeout:
             print('Polling time exceeded')
-        # except ConnectionError as err:
-        #     for n in range(3):
-        #         sleep(30)
-        #         if n <= 3:
-        #             response = get_polling(api_key, last_attempt_timestamp)
-        #             await send_message(bot, tg_user_id, response)
-        #         else:
-        #             print(err)
+        except ConnectionError as err:
+            for n in range(3):
+                sleep(30)
+                if n <= 3:
+                    response = get_polling(api_key, last_attempt_timestamp)
+                    await send_message(bot, tg_user_id, response)
+                else:
+                    print(err)
 
 
 asyncio.run(main())
